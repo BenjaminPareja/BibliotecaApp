424eab87d63ca7ea0071f38ce30c98c4378f9db3
Brian pareja <brian.pareja@tecsup.edu.pe>
2017-11-06 11:44:06 -0500

    Mi nuevo commit

---
+++ BibliotecaApp/Controllers/LibroController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BibliotecaApp.Models;
+
+namespace BibliotecaApp.Controllers
+{
+    public class LibroController : Controller
+    {
+        // GET: Libro
+        public ActionResult Index()
+        {
+            List<Libro> libros = new List<Libro>();
+            libros.Add(new Libro
+            {
+                ID = 1,
+                Nombre = "Inferno",
+                Autor = "Dan Brown"
+            });
+            libros.Add(new Libro
+            {
+                ID = 1,
+                Nombre = "Danza de dragones",
+                Autor = "George el curioso"
+            });
+            libros.Add(new Libro
+            {
+                ID = 1,
+                Nombre = "La divina comedia",
+                Autor = "Dante Aliguiari"  //un nuevo comit
+            });
+            return View(libros);
+        }
+    }
+}
---
+++ BibliotecaApp/Models/Libro.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BibliotecaApp.Models
+{
+    public class Libro
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Autor { get; set; }
+    }
+}
